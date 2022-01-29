ea9fe1dc4c58f60b023034a0d3e06b8d6ee33700
Volod878 <volod1988@gmail.com>
2022-01-29 22:24:25 +0700

    Init

---
+++ src/test/java/ru/volod878/english/EnglishApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.volod878.english;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
