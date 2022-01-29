bbb47f6d2f76675d827b1960f3b216a4a2a44abc
Volod878 <volod1988@gmail.com>
2022-01-29 23:13:31 +0700

    refactored mp3 parser

--- src/main/java/ru/volod878/english/util/ParserMp3.java
+++ src/main/java/ru/volod878/english/util/ParserMp3.java
@@ -1,27 +1,37 @@
 package ru.volod878.english.util;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.net.URL;
 
 public class ParserMp3 {
-    public static void main(String[] args) {
-        try{
-            BufferedInputStream in = new BufferedInputStream(new URL("https://wooordhunt.ru/data/sound/sow/us/consumer.mp3").openStream());
-            FileOutputStream fos = new FileOutputStream("sound.mp3");
-            BufferedOutputStream bout = new BufferedOutputStream(fos,1024);
+    @Value("${url-sound-us}")
+    private static String urlSoundUs;
+    @Value("${url-sound-uk}")
+    private static String urlSoundUk;
+
+    public static void parseUs(String word) {
+        parse(urlSoundUs, word);
+    }
+
+    public static void parseUk(String word) {
+        parse(urlSoundUk, word);
+    }
+
+    private static void parse(String urlSound, String word) {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(urlSound + "/" + word + ".mp3").openStream());
+             FileOutputStream fos = new FileOutputStream(word + ".mp3");
+             BufferedOutputStream bout = new BufferedOutputStream(fos, 1024)) {
+
             byte[] data = new byte[1024];
             int x;
-            while((x=in.read(data,0,1024))>=0){
-                bout.write(data,0,x);
+            while ((x = in.read(data, 0, 1024)) >= 0) {
+                bout.write(data, 0, x);
             }
-            fos.flush();
-            bout.flush();
-            fos.close();
-            bout.close();
-            in.close();
+        } catch (Exception ignored) {
         }
-        catch (Exception ignored) {}
     }
 }
