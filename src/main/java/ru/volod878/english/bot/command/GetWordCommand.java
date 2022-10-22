9e478d3b7373b2f6fcd9a577e8ff0b115cabb981
Volod878 <volod1988@gmail.com>
2022-10-22 22:09:35 +0700

    upd readme

--- src/main/java/ru/volod878/english/bot/command/GetWordCommand.java
+++ src/main/java/ru/volod878/english/bot/command/GetWordCommand.java
@@ -14,13 +14,22 @@ public class GetWordCommand implements Command {
     private final ISendBotMessage sendBotMessage;
     private final IVocabularyService vocabularyService;
 
+    /**
+     * Регулярное выражение:
+     * Строка должна начинаться и заканчиваться символом англ алфавита
+     * Строка должна содержать только одно слово
+     */
+    private final static String ONLY_TEXT_PATTERN = "^[a-zA-Z]+$";
+
     @Override
     public void execute(Update update) {
-        log.info("the GET_WORD command is executed");
-        VocabularyDto dto = vocabularyService.getOrCreateByWord(update.getMessage().getText());
-        String chatId = update.getMessage().getChatId().toString();
-        sendBotMessage.sendMessage(chatId, dto.toString());
-        sendBotMessage.sendVoice(chatId, vocabularyService.getMp3File(Location.US, dto.getWord()));
-        sendBotMessage.sendVoice(chatId, vocabularyService.getMp3File(Location.UK, dto.getWord()));
+        if (update.getMessage().getText().matches(ONLY_TEXT_PATTERN)) {
+            log.info("the GET_WORD command is executed");
+            VocabularyDto dto = vocabularyService.getOrCreateByWord(update.getMessage().getText());
+            String chatId = update.getMessage().getChatId().toString();
+            sendBotMessage.sendMessage(chatId, dto.toString());
+            sendBotMessage.sendVoice(chatId, vocabularyService.getMp3File(Location.US, dto.getWord()));
+            sendBotMessage.sendVoice(chatId, vocabularyService.getMp3File(Location.UK, dto.getWord()));
+        }
     }
 }
